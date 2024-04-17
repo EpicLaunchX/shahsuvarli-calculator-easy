953f13e7a1d948f7da1e811fc1f1fe4bfeefe433
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2024-04-18 00:15:37 +0200

    fix: ran unit tests in local

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -1,4 +1,6 @@
 class Operands:
+    __slots__ = ["first_operand", "second_operand"]
+
     def __init__(self, first_operand: int, second_operand: int):
         self.first_operand = first_operand
         self.second_operand = second_operand
