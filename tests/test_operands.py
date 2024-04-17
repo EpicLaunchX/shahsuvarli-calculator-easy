6e3203a809f3d49410b6ecb89c2a3ee7471c9558
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2024-04-17 23:53:20 +0200

    Task 1 completed

---
+++ tests/test_operands.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestOperands(unittest.TestCase):
+    def test_init(self):
+        operands = Operands(5, 10)
+        self.assertEqual(operands.first_operand, 5)
+        self.assertEqual(operands.second_operand, 10)
+
+if __name__ == '__main__':
+    unittest.main()
